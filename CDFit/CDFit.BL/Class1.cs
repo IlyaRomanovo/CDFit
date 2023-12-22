2d8f9e93ae81203188a4dda1e82e98dac32ac946
Илья Потапенко <ilap16130@gmail.com>
2023-12-23 00:47:49 +0300

    Создана модель Пользователя

--- CDFit/CDFit.BL/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CDFit.BL
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CDFit/CDFit.BL/Model/Gender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CDFit.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создание нового пола.   
+        /// </summary>
+        /// <param name="name"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender (string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или null", nameof(name));
+            }    
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
